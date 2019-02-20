3fb61602d9ebd88775dd142905f04011bc0697d3
Joe Friesen <friesen.josephc@gmail.com>
2019-02-19 16:24:02 -0800

    Rework to listen to any updates.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ class App extends React.Component {
     super(props)
     this.state = {
       ready: false,
+      // remote
+      // endpoint: 'walkertrekker.herokuapp.com',
+      // playerId: 'ec8b81d6-ae17-4de9-99ca-fe007f71f731',
+      // local
       endpoint: 'localhost:5000',
       playerId: '7dd089c0-7f4b-4f39-a662-53554834a8f7',
     }
@@ -16,39 +20,46 @@ class App extends React.Component {
     client.connect()
     client.on('connect', () => {
       console.log('Emiting findPlayerInfo event')
-      client.emit('findPlayerInfo', this.state.playerId)
+      client.emit('getPlayerInfo', this.state.playerId)
     })
-    client.on('playerInfoFound', (player) => {
+    client.on('sendPlayerInfo', (player) => {
       console.log('playerInfoFound event received, data: ')
       console.log(player)
       this.setState({ player })
       if (player.inActiveGame) {
-        client.emit('findCampaignInfo', player.campaignId)
-      } else {
-        this.setState({ ready: true })
+        client.emit('getCampaignInfo', player.campaignId)
       }
     })
-    client.on('campaignInfoFound', (campaign) => {
+    client.on('sendCampaignInfo', (campaign) => {
       console.log('campaignInfoFound event received, data: ')
       console.log(campaign)
       this.setState({ campaign, ready: true })
     })
-    client.on('log', (data) => {
-      console.log(data)
+    client.on('log', (msg) => {
+      console.log(msg)
     })
+  }
+
+  componentWillUnmount() {
+    const client = io(this.state.endpoint)
+    client.emit('disconnect')
   }
 
   render() {
     if (this.state.ready) {
+      const active = this.state.player.inActiveGame ? 'Yes' : 'No'
       return (
         <div>
           <ul>
             <li> Player ID: {this.state.playerId} </li>
-            <li> Display Name {this.state.player.displayName} </li>
-            <li> In Active Game {this.state.player.inActiveGame} </li>
+            <li> Display Name: {this.state.player.displayName} </li>
+            <li> In Active Game: {active} </li>
             <li> Campaign ID: {this.state.player.campaignId} </li>
             <li> Start Date: {this.state.campaign.startDate} </li>
             <li> Campaign Length: {this.state.campaign.length} </li>
+            <li> Party Food Items: {this.state.campaign.inventory.foodItems} </li>
+            <li> Party Medicine Items: {this.state.campaign.inventory.medicineItems} </li>
+            <li> Party Weapon Items: {this.state.campaign.inventory.weaponItems} </li>
             <li> Players:
               <ul>
                 {this.state.campaign.players.map(player => {
@@ -63,7 +74,7 @@ class App extends React.Component {
       );
     }
     else {
-      return (<div>Waiting...</div>)
+      return (<div>Fetching data...</div>)
     }
   }
 
